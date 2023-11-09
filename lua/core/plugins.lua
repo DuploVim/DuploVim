01bebc02273c3fa6fcf82c8c530e819c634f0e8a
ProggerX <x@proggers.ru>
2023-11-09 16:52:29 +0300

    Added lazygit

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -68,4 +68,11 @@ require("lazy").setup({
       require("refactoring").setup()
     end,
   },
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
 })
