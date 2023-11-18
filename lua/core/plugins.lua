eca1506b9ece4c58b7fdb51957342206ecdcbad2
ProggerX <x@proggers.ru>
2023-11-18 21:34:44 +0300

    Delete xmake

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -91,14 +91,6 @@ require("lazy").setup({
 	},
 	{'akinsho/toggleterm.nvim', version = "*", config = true },
 	{
-    	"Mythos-404/xmake.nvim",
-		branch = "v1",
-    	lazy = true,
-    	event = "BufReadPost xmake.lua",
-		config = true,
-    	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	},
-	{
     	'nvim-telescope/telescope.nvim', tag = '0.1.4',
     	dependencies = { 'nvim-lua/plenary.nvim' }
     },
