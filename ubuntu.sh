e0c709a40a364395f2581ca8584acec7c9ad4636
ProggerX <x@proggers.ru>
2023-11-18 20:07:43 +0300

    Added ubuntu script

---
+++ ubuntu.sh
@@ -0,0 +1,11 @@
+sudo apt install -y git gcc ripgrep unzip ccls curl python3 python3-pip
+sudo snap install pyright --classic
+
+wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod +x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv ./squashfs-root/* /
+
+git clone https://github.com/DuploVim/DuploVim ~/.config/nvim 
+
+echo "DONE, RUN 'nvim' command to run DuploVim"
