115dac87e623e793621ac7aaae2146b9a01c4a41
ProggerX <x@proggers.ru>
2023-11-19 21:50:00 +0300

    nvim-surround fix

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -102,5 +102,12 @@ require("lazy").setup({
     	end
 	},
 	{ 'mg979/vim-visual-multi' },
-	{ 'kylechui/nvim-surround' },
+	{
+    	"kylechui/nvim-surround",
+    	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    	event = "VeryLazy",
+    	config = function()
+			require("nvim-surround").setup({})
+    	end
+	}
 })
