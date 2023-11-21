360a3a729ca08b501356fc1390afdabcdd5c5a95
ProggerX <x@proggers.ru>
2023-11-21 15:11:06 +0300

    Added xmake c++ neoproj template

--- lua/plugins/neoproj.lua
+++ lua/plugins/neoproj.lua
@@ -4,3 +4,8 @@ require "neoproj".setup {
   -- Directory which contains all of your projects
   project_path = path,
 }
+
+require "neoproj".register_template {
+	name = "Xmake C++ project",
+	expand = "xmake create -P . && xmake project -k compile_commands --lsp=ccls",
+}
