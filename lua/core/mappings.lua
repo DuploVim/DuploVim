d16a610846413480dd4a7c4d51675f0e66192543
ProggerX <x@proggers.ru>
2023-12-08 01:36:09 +0300

    Changed oil shortcut

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,9 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- FileManagers
 vim.keymap.set('n', '<leader>e', '<CMD>Neotree float<CR>')
 vim.keymap.set('n', '<leader>E', '<CMD>Neotree left<CR>')
-vim.keymap.set('n', '<leader>o', '<CMD>Oil<CR>')
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- Tabs
 vim.keymap.set('n', '<A-Tab>', '<CMD>BufferNext<CR>')
@@ -12,14 +12,14 @@ vim.keymap.set('n', '<A-,>', '<CMD>BufferPrevious<CR>')
 vim.keymap.set('n', '<C-W>', '<CMD>BufferClose<CR>')
 
 -- Terminal
-vim.keymap.set('n', '<leader>h', '<CMD>ToggleTerm direction=horizontal<CR>', {desc = "Open horizontal terminal"})
-vim.keymap.set('n', '<leader>v', '<CMD>ToggleTerm direction=vertical<CR>', {desc = "Open vertivel terminal"})
-vim.keymap.set('n', '<leader>f', '<CMD>ToggleTerm direction=float<CR>', {desc = "Open floating terminal"})
+vim.keymap.set('n', '<leader>h', '<CMD>ToggleTerm direction=horizontal<CR>', {desc = 'Open horizontal terminal'})
+vim.keymap.set('n', '<leader>v', '<CMD>ToggleTerm direction=vertical<CR>', {desc = 'Open vertivel terminal'})
+vim.keymap.set('n', '<leader>f', '<CMD>ToggleTerm direction=float<CR>', {desc = 'Open floating terminal'})
 vim.keymap.set('t', '<C-R>', '<C-\\><C-n>')
 
 -- Telescope
-vim.keymap.set('n', 'tf', '<CMD>Telescope find_files<CR>', {desc = "Search files"})
-vim.keymap.set('n', 'tg', '<CMD>Telescope live_grep<CR>', {desc = "Telescope live grep"})
+vim.keymap.set('n', 'tf', '<CMD>Telescope find_files<CR>', {desc = 'Search files'})
+vim.keymap.set('n', 'tg', '<CMD>Telescope live_grep<CR>', {desc = 'Telescope live grep'})
 
 
 -- Other
